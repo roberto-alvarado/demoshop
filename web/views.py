63ee333dea538d0297cd1b49fe18e3a1703f6c9d
Roberto Alvarado <rcalvaradom@gmail.com>
2023-12-13 08:30:16 -0600

    Modulo 3

--- web/views.py
+++ web/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Categoria,Producto
+
+from .carrito import Cart
 
 # Create your views here.
 """ VISTAS PARA EL CATALOGO DE PRODUCTOS"""
@@ -59,3 +61,35 @@ def productoDetalle(request,producto_id):
     return render(request,'producto.html',context)
 
 
+""" VISTAS PARA EL CARRITO DE COMPRAS """
+
+def carrito(request):
+    return render(request, 'carrito.html')
+
+def agregarCarrito(request,producto_id):
+    if request.method == 'POST':
+        cantidad = int(request.POST['cantidad'])
+    else:         
+        cantidad = 1
+
+    objProducto = Producto.objects.get(pk=producto_id)
+    carritoProducto = Cart(request)
+    carritoProducto.add(objProducto,cantidad)
+
+    if request.method == 'GET':
+        return redirect('/')
+
+    return render(request,'carrito.html')
+
+def eliminarProductoCarrito(request,producto_id):
+    objProducto = Producto.objects.get(pk=producto_id)
+    carritoProducto = Cart(request)
+    carritoProducto.delete(objProducto)
+
+    return render(request, 'carrito.html')
+
+def limpiarCarrito(request):
+    carritoProducto = Cart(request)
+    carritoProducto.clear()
+
+    return render(request, 'carrito.html')
