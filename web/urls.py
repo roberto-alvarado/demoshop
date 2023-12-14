e22f8e1d1ca9f9396159b2e016ce67a14f439a3b
Roberto Alvarado <rcalvaradom@gmail.com>
2023-12-14 07:34:58 -0600

    MODULO 4

--- web/urls.py
+++ web/urls.py
@@ -5,12 +5,18 @@ from . import views
 app_name = 'web'
 
 urlpatterns = [
-    path('', views.index,name = 'index'),
+    path('',views.index,name='index'),
     path('productosPorCategoria/<int:categoria_id>',views.productosPorCategoria,name='productosPorCategoria'),
     path('productosPorNombre', views.productosPorNombre,name='productosPorNombre'),
     path('producto/<int:producto_id>',views.productoDetalle,name='producto'),
     path('carrito',views.carrito,name='carrito'),
     path('agregarCarrito/<int:producto_id>',views.agregarCarrito,name='agregarCarrito'),
     path('eliminarProductoCarrito/<int:producto_id>',views.eliminarProductoCarrito,name='eliminarProductoCarrito'),
-    path('limpiarCarrito',views.limpiarCarrito,name='limpiarCarrito')
+    path('limpiarCarrito',views.limpiarCarrito,name='limpiarCarrito'),
+    path('crearUsuario',views.crearUsuario,name='crearUsuario'),
+    path('cuenta',views.cuentaUsuario,name='cuentaUsuario'),
+    path('actualizarcliente',views.actualizarCliente,name='actualizarCliente'),
+    path('login',views.loginUsuario,name='loginUsuario'),
+    path('logout',views.logoutUsuario,name='logoutUsuario'),
+    path('registrarPedido',views.registrarPedido,name='registrarPedido')
 ] 
